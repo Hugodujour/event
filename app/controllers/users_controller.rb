a7dcc30c0a6464f21e337d595e04f1e709798f88
Hugodujour <hugogreg@live.fr>
2022-11-08 15:30:48 +0100

    user functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to @user, notice: 'User was successfully gneugneugneu'
+		else
+			dlash[:danger] = 'Something went wrong'
+			render :new
+		end
+	end
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :description)
+	end
+
+end
+
+
+
+
+
+
+
+
+
+
 end
